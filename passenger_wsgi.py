a29bed4a7cf265b92b2de5899bb5493e41ebdd89
crabhub19 <3xbishal@gmail.com>
2025-07-17 12:47:01 +0600

    a

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -2,7 +2,7 @@ import os
 import sys
 sys.path.insert(0, os.path.dirname(__file__))
 # Set the Django settings module
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'attendance_system.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sms.settings')
 # Import the application object
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
