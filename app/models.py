8faa0990fec891dbd676ac16c7ce1e2088a158f2
Bishal <3xbishal@gmail.com>
2025-08-30 16:19:08 +0600

    a

--- app/models.py
+++ app/models.py
@@ -80,7 +80,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
 
 class Location(BaseModel):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
